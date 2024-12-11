d941ce141a5738d15458a8b70eb0655d5cea61f9
Anita <at9941787@gmail.com>
2024-12-11 09:44:35 +0545

    first commit

---
+++ android/app/src/main/kotlin/Anita/com/flutter_calculator_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package Anita.com.flutter_calculator_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calculator_project/app.dart';
+
+// final
+// const
+
+// HOT RELOAD
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
